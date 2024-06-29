e7d3d6754ac0debfc4c97c572cb7c3835c8da9c4
HassanShanJava <hassanshanqsols@gmail.com>
2024-06-30 02:43:38 +0500

    worked on hero scetion

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="min-h-[60vh] text-white flex flex-col-reverse lg:flex-row gap-6 sm:gap-10 lg:gap-0 items-center justify-between">
+      <div className="space-y-10">
+        <h1 className="text-4xl lg:text-7xl font-bold">
+          Nice to meet you 👋🏻 <br /> <span className="underline underline-offset-8 decoration-green-500">I'm Hassan</span>
+        </h1>
+        <p className="md:w-96 text-lg  text-gray-300   ">
+          Based in Pakistan, I'm a web developer passionate about building a
+          modern web application that users love
+        </p>
 
-export default Hero
+        <Link href={"mailto:hassanshan675@gmail.com"} className="group inline-block">
+          <h1 className="text-3xl font-bold group-hover:text-green-400 transition-all ">Contact me 📬</h1>
+          <div className="h-2 w-40 bg-green-500 rounded-full -translate-x-2"></div>
+          <div className="h-2 w-40 bg-indigo-500 rounded-full "></div>
+        </Link>
+      </div>
+
+      <div className="">
+        <div className="w-72 h-72 space-y-3 -rotate-[30deg] relative"> 
+          <div className="flex gap-3 translate-x-8">
+            <div className="w-32 h-32 rounded-2xl bg-green-500"></div>
+            <div className="w-32 h-32 rounded-full bg-indigo-500"></div>
+          </div>
+          
+          <div className="flex gap-3 -translate-x-8">
+            <div className="w-32 h-32 rounded-2xl bg-indigo-500"></div>
+            <div className="w-32 h-32 rounded-full bg-green-500"></div>
+          </div>
+          
+          <div className="glow-effect absolute top-[30%] right-1/2 -z-10"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,10 +3,13 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black">
-      <div className="max-w-7xl mx-auto">
+    <div className="min-h-screen bg-black overflow-hidden">
+      <div className="dark:bg-black  dark:bg-grid-white/[0.2] bg-grid-black/[0.2]">
+
+      <div className="max-w-7xl mx-auto p-5">
         <Navbar />
         <Hero/>
+      </div>
       </div>
     </div>
   );
